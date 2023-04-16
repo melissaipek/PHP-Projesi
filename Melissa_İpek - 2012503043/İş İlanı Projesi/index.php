de4abbc067d1cdf23e293dd5cc828889d12c079d
Melissa <129597862+Melissaipek@users.noreply.github.com>
2023-04-16 16:04:08 +0300

    PHP Kullanıcı Giriş ve İlan Kayıt
    Veri tabanıyla oluşturduğum ve PHP ve biraz da temel html, css kullanarak hazırladığım proje ektedir. Projemizde iş ilanı giriş ekranı ve sistemi mevcuttur.

---
+++ Melissa_İpek - 2012503043/İş İlanı Projesi/index.php
@@ -0,0 +1,23 @@
+
+<!-- Burda -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+	<title>Ana Sayfa</title>
+</head>
+<body>
+
+	<center>
+	<h1>HOŞGELDİNİZ</h1>
+	<hr>
+
+	<p>İş İlanına Başvurmak İçin <a href="kullanicigiris.php">Tıklayınız</a></p>
+	<p>İş İlanı Vermek İçin <a href="admingiris.php">Tıklayınız</a></p>
+    <hr>
+</center>
+</body>
+</html>
